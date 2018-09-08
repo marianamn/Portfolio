1fd220ce74b657a0aadc7e7b66b2e89d5431ff0c
marianamn <mariananaidenova@mail.bg>
2018-09-08 14:56:30 +0300

    setup: add heroku credentials

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,10 +1,12 @@
-const connection = 'mongodb://localhost/portfolio';
-const portToRun = 3001;
-const urlToRun = `http://localhost${portToRun}`;
+const connection = process.env.MONGODB_URI || 'mongodb://localhost/portfolio';
+const portToRun = process.env.PORT || 3001;
+const url = process.env.NODE_ENV || `http://localhost${portToRun}`;
+let herokuConnectionString = "mongodb://heroku_29ccp20n:3nl1m17vou5482l5h7ij97erof@ds249092.mlab.com:49092/heroku_29ccp20n";
 
 module.exports = {
-    'connectionString': connection,
-    'port': portToRun,
-    'url': urlToRun,
-    'secret': 'yourSecret'
+    connectionString: connection,
+    port: portToRun,
+    url,
+    secret: 'yourSecret',
+    herokuConnectionString
 };
